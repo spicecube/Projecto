0de34181c60b0a6343fde4a2a060b2961b0ff1bf
Alfred <Alfred@AlfredPC>
2015-03-29 14:58:29 -0500

    Add two types of "scenes"
    Scene-	handles just a scene with pic and dialog
    Diverenge-	Scenes that have a selection that determines the next outcome

---
+++ src/contents/Divergence.java
@@ -0,0 +1,18 @@
+//These are the scenes where the options are shown
+//
+
+package contents;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+//For now, it extends Scene because it should have all its qualities plus selections
+public class Divergence extends Scene
+{
+	
+	Divergence(ImageIcon picture, String dialog)
+	{
+		super(picture, dialog);
+	}
+
+}
---
+++ src/contents/Scene.java
@@ -0,0 +1,17 @@
+//Plain scene that the just shows a picture with dialog
+//maybe animation?
+
+package contents;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Scene extends JPanel
+{
+	
+	Scene(ImageIcon picture, String dialog)
+	{
+		
+	}
+
+}
